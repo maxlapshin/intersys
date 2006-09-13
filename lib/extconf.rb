f6e70ca633c5fe50433dca8ee99230a7156cd3ce
max <max@38fedb62-f319-0410-8306-f67a23000141>
2006-09-13 19:02:46 +0000

    Compilling on linux
    git-svn-id: http://svn.maxidoors.ru/cache-ruby/trunk@51 38fedb62-f319-0410-8306-f67a23000141

--- lib/extconf.rb
+++ lib/extconf.rb
@@ -13,14 +13,14 @@ CONFIG["CC"] = "gcc -g"
 #EOF
 #end
 
-@cache_placements = ["/Applications/Cache", "/cygdrive/c/Progra~1/Cache", "/cygdrive/c/Cachesys"]
+@cache_placements = ["/home/max/cache", "/Applications/Cache", "/cygdrive/c/Progra~1/Cache", "/cygdrive/c/Cachesys"]
 
 def locations(suffix)
   @cache_placements.map {|place| place + suffix}
 end
 
 def include_locations
-  locations("/dev/cpp/include")
+  locations("/dev/cpp/include") + ["./sql_include"]
 end
 
 def library_locations
@@ -31,8 +31,14 @@ end
 def include_flags
   " "+(include_locations.map { |place| "-I"+place} + ["-Wall"]).join(" ")
 end
+def link_flags
+  " "+(library_locations.map { |place| "-L"+place} + ["-Wall"]).join(" ")
+end
 find_header "c_api.h", *include_locations
+find_header "sql.h", *include_locations
+find_header "sqlext.h", *include_locations
 $CFLAGS << include_flags
+$LDFLAGS << link_flags
 #$LDFLAGS << "-Wl,-no-export-libs,cbind.lib"
 find_library "cbind", "cbind_alloc_db",*library_locations
 create_makefile 'intersys_cache'
