ba20b67964ebf2bb826c561a367f3c2ba3c6b5cd
krasin <krasin@38fedb62-f319-0410-8306-f67a23000141>
2006-10-08 16:48:29 +0000

    Исправлена сборка для Windows и MS VS 8.0. Частично убраны зависимости от места, в которое установлена VS.
    git-svn-id: http://svn.maxidoors.ru/cache-ruby/trunk@62 38fedb62-f319-0410-8306-f67a23000141

--- lib/extconf.rb
+++ lib/extconf.rb
@@ -2,10 +2,11 @@
 
 require "mkmf"
 
-CONFIG["CC"] = "gcc -g" unless RUBY_PLATFORM.match /mswin/
-
 WIN32 = RUBY_PLATFORM.match(/mswin/)
 MACOS = RUBY_PLATFORM.match(/darwin/)
+
+CONFIG["CC"] = "gcc -g" unless WIN32
+
 
 #alias :old_cpp_include :cpp_include
 #def cpp_include(header)
@@ -15,7 +16,7 @@ MACOS = RUBY_PLATFORM.match(/darwin/)
 #EOF
 #end
 
-@cache_placements = ["/home/max/cache", "/Applications/Cache", "/cygdrive/c/Progra~1/Cache", "/cygdrive/c/Cachesys", "C:/Cachesys"]
+@cache_placements = ["/home/max/cache", "/Applications/Cache", "/cygdrive/c/Progra~1/Cache", "/cygdrive/c/Cachesys", "C:/Cachesys", 'C:/Program Files (x86)/Cache']
 
 def locations(suffix)
   # .map{|place| place.split("/").join(WIN32 ? "\\" : "/")}
@@ -39,9 +40,25 @@ if WIN32
   $CFLAGS << ' -I"C:\\Program Files\\Microsoft Platform SDK\\Include\\crt" '
   $CFLAGS << ' -I"C:\\Program Files\\Microsoft Platform SDK\\Include" '
   $CFLAGS << ' -I"C:\\Program Files\\Microsoft Visual Studio 8\\VC\\include" -D_WIN32 '
-  $LDFLAGS << ' /link -libpath:"C:/CacheSys/dev/cpp/lib" '
-  $LDFLAGS << ' -libpath:"C:/Program Files/Microsoft Visual Studio 8/VC/lib" '
-  $LDFLAGS << ' -libpath:"C:/Program Files/Microsoft Platform SDK/Lib" '
+  
+  $LDFLAGS << ' cbind.lib '
+  
+  VCINSTALLDIR = ENV["VCINSTALLDIR"]
+  
+  if VCINSTALLDIR.nil? then 
+	$LDFLAGS << ' -libpath:"C:/Program Files (x86)/Microsoft Visual Studio 8/VC/lib" '
+	$LDFLAGS << ' -libpath:"C:/Program Files/Microsoft Visual Studio 8/VC/lib" '  
+  else 
+    $LDFLAGS << ' -libpath:"' + VCINSTALLDIR + '" '
+  end
+  
+  LIB = ENV["lib"]
+  unless LIB
+    LIB.split(";").each{ |path| $LDFLAGS << ' -libpath:"' + path.strip + '" ' }
+  end  
+
+  $LDFLAGS << ' -libpath:"C:/CacheSys/dev/cpp/lib" '
+  $LDFLAGS << ' -libpath:"C:/Program Files (x86)/Cache/dev/cpp/lib" '
 end
 
 
@@ -52,6 +69,7 @@ end
 $CFLAGS << include_flags
 $LDFLAGS << link_flags
 
+
 have_header "c_api.h"
 unless have_header "sql.h" 
   $CFLAGS << ' -Isql_include '
@@ -61,6 +79,12 @@ have_header "sqlext.h"
 
 unless MACOS
   find_library "cbind", "cbind_alloc_db",*library_locations
+end
+
+if WIN32
+	CONFIG["LDSHARED"] = "link" 
+	CONFIG["prefix"] = '"' + CONFIG["prefix"] + '"' 
+	$ruby = '"' + $ruby + '"' 	
 end
 
 
