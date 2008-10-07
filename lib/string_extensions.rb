8ad3b9d6eff9240644105defb9c8a5925b2ab6c0
Anton Dyachuk <anton.dyachuk@gmail.com>
2008-10-07 13:46:56 +0400

    bugfix: camelize(:lower) work incorrectly

--- lib/string_extensions.rb
+++ lib/string_extensions.rb
@@ -12,16 +12,9 @@ module Intersys
     #   "active_record/errors".camelize         # => "ActiveRecord::Errors"
     #   "active_record/errors".camelize(:lower) # => "activeRecord::Errors"
     def camelize(first_letter = :upper)
-            # def camelize(lower_case_and_underscored_word, first_letter_in_uppercase = true)
-#         if first_letter_in_uppercase
-#           lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
-#         else
-#           lower_case_and_underscored_word.first.downcase + camelize(lower_case_and_underscored_word)[1..-1]
-#         end
-#       end
       case first_letter
         when :upper then self.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
-        when :lower then self.first.downcase + self[1..-1].camelize
+        when :lower then self.first.downcase + self.camelize[1..-1]
       end
     end
     
